aae1769ccafceb42ec16057e4c2f8874ed8df441
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-02-12 15:34:46 +0100

    added email icon on small screens

--- src/Sections/Footer.tsx
+++ src/Sections/Footer.tsx
@@ -1,4 +1,6 @@
+import email from "../assets/sms full.svg";
 import { MediumBlogButton } from "../components/MediumBlogButton";
+import SocialMediaIcon from "../components/SocialMediaIcon";
 import SocialMediaIcons from "../components/SocialMediaIcons";
 
 function Footer() {
@@ -7,6 +9,12 @@ function Footer() {
             <div className={"flex gap-4 align-middle h-full w-full pr-0 mr-0"}>
                 <div className="mt-auto mb-auto flex gap-4">
                     <SocialMediaIcons height={6} />
+                    <SocialMediaIcon
+                        link={"mailto:boudaakkarnazih@gmail.com"}
+                        img={email}
+                        alt="email icon"
+                        height={6}
+                    />
                 </div>
                 <MediumBlogButton />
             </div>
