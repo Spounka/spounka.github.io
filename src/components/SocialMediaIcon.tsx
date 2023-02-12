43143b5d744b336d5c2211370ffb729dd42b0d96
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-02-12 12:56:31 +0100

    fixed minor issues

--- src/components/SocialMediaIcon.tsx
+++ src/components/SocialMediaIcon.tsx
@@ -1,6 +1,6 @@
 function SocialMediaIcon({ height, link, alt, img }: any) {
     return (
-        <a href={link} className={"p-0 m-0"}>
+        <a href={link} className={"p-0 m-0"} target="_blank">
             <img src={img} alt={alt} className={`w-${height} h-${height} p-0 m-0`} />
         </a>
     );
--- src/components/TextWithLineUnder.tsx
+++ src/components/TextWithLineUnder.tsx
@@ -2,7 +2,7 @@ import line from "../assets/Straight Line.svg";
 
 function TextWithLineUnder({ children }: any) {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col justify-start items-start">
             {children}
             <img src={line} alt="straight line svg" className="h-3 max-w-xs" />
         </div>
